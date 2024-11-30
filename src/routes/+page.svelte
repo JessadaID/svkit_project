b574db5edca76c5f666eb47c364126a8605837a2
jessada <ipnioko54@gmail.com>
2024-11-30 11:05:34 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let data
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<div>
+    {#each data.blogs as blog}
+        <li>{blog.id} Name : {blog.name}</li>
+        <a href="/blog/{blog.id}">view</a>
+    {/each}
+</div>
